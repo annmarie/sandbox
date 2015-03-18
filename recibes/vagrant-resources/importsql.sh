981d1e81d5c26393025b02634f0239389580d6d4
annmarie <annmarie.daylife@gmail.com>
2015-03-18 11:39:15 -0400

    added sample data

--- recibes/vagrant-resources/importsql.sh
+++ recibes/vagrant-resources/importsql.sh
@@ -1,10 +1,10 @@
 
 if [ ! -z `ls $1` ]; then
     # Create a database with that name
-    mysqladmin -u root -h 172.12.12.12 -f drop recibes
-    mysqladmin -u root -h 172.12.12.12 create recibes
+    mysqladmin -u root -h 172.12.12.12 -proot -f drop recibes
+    mysqladmin -u root -h 172.12.12.12 -proot create recibes
     
     # Import the SQL into new database
-    mysql -u root -f -h 172.12.12.12 recibes < $1 
+    mysql -u root -f -h 172.12.12.12 -proot recibes < $1 
 fi                      
 
